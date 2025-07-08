bbce9b6b2ebb82b9f4f3cb22492be161e3a422a0
gianmfed <72508321+gianmfed@users.noreply.github.com>
2025-07-08 16:53:08 +0200

    first commit

---
+++ js/formhandler.js
@@ -0,0 +1,60 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  // Prevent the default form submission action
+  event.preventDefault()
+  // Get the form data
+  const formData = new FormData(event.target);
+  var message = formData.get('message');
+  // console.log(message)  
+  ////////////////////////////
+  const button = document.getElementById('submitButton');
+
+  button.classList.remove('d-none');
+  button.innerHTML = 'Sending';
+
+  // setInterval(function() { // ritardare l'esecuzione
+  ////////////////////////////
+
+  // Send the form data to the server via AJAX
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://formsubmit.co/d1f5b81bd3827b70a6b3f109b150aabc');
+  xhr.send(formData);
+  // Handle the server response
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+        // The form was submitted successfully
+        // Update the page or display a success message
+
+        const response = xhr.response;
+        // console.log(response)
+        const pattern = /Unable to submit form/i;
+        if (pattern.test(response)) {
+          // const button = document.getElementById('submitButton');
+
+          button.innerHTML = 'Something went wrong! Retry';
+
+          button.classList.remove('btn-primary');
+          button.classList.add('btn-danger');
+        } else {
+          // const button = document.getElementById('submitButton');
+
+          button.innerHTML = 'Thankyou!';
+
+          button.classList.remove('btn-primary');
+          button.classList.add('btn-success');
+          button.setAttribute('type', 'button');
+        }
+      } else {
+        // There was an error submitting the form
+        // Display an error message or update the page
+        const button = document.getElementById('submitButton');
+
+        button.innerHTML = 'Something went wrong! Retry';
+
+        button.classList.remove('btn-primary');
+        button.classList.add('btn-danger');
+      }
+    }
+
+// }, 5000); // Appartiene a setInterval
+  }
+);
